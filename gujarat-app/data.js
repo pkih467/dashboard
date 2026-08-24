07a4d1be160adce5b6ca5e9ecb614203eeaeabe8
pkih467 <outsets-critic-2h@icloud.com>
2026-08-24 21:03:17 +0530

    Update Gujarat data with new districts and log message

--- gujarat-app/data.js
+++ gujarat-app/data.js
@@ -5,24 +5,19 @@
 const GujaratRelationalData = {
   districts: [
     {
+      name: "Valsad",
+      assemblies: ["177 - Gandevi", "178 - Vansda", "179 - Dharampur", "180 - Valsad", "181 - Pardi", "182 - Umbergaon"],
+      talukas: ["Valsad", "Pardi", "Umbergaon", "Vapi", "Dharampur", "Kaprada"]
+    },
+    {
       name: "Kachchh",
       assemblies: ["1 - Anjar", "2 - Bhuj", "3 - Gandhidham", "4 - Rapar", "5 - Mandvi", "6 - Bhachau", "7 - Abdasa"],
       talukas: ["Bhuj", "Anjar", "Gandhidham", "Rapar", "Mandvi", "Nakhatrana", "Lakhpat", "Abdasa"]
     },
     {
-      name: "Banaskantha",
-      assemblies: ["11 - Vadgam", "12 - Palanpur", "13 - Deesa", "14 - Dhanera"],
-      talukas: ["Palanpur", "Deesa", "Dhanera", "Vav", "Tharad", "Kankrej", "Deodar"]
-    },
-    {
       name: "Ahmedabad",
       assemblies: ["55 - Ellisbridge", "56 - Danilimda", "57 - Sabarmati", "58 - Asarwa", "61 - Maninagar", "67 - Vejalpur", "68 - Vatva", "71 - Ghatlodia"],
       talukas: ["Ahmedabad City", "Sanand", "Daskroi", "Dhandhuka", "Viramgam", "Bavla", "Dholka"]
-    },
-    {
-      name: "Rajkot",
-      assemblies: ["7 - Morvi", "10 - Rajkot East", "11 - Rajkot West", "12 - Rajkot South", "13 - Rajkot Rural", "14 - Jasdan", "15 - Gondal"],
-      talukas: ["Rajkot", "Jetpur", "Gondal", "Jasdan", "Dhoraji", "Upleta", "Lodhika", "Kotda Sangani"]
     },
     {
       name: "Surat",
@@ -32,4 +27,4 @@ const GujaratRelationalData = {
   ]
 };
 
-console.log("Gujarat Relational Data Loaded.");
+console.log("Gujarat Relational Data Loaded Successfully.");
